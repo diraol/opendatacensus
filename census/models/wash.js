64c1a8d947f797d024257bb013642fa0f4ef1b37
Luis Leao <luis.leao@gmail.com>
2015-12-17 11:30:24 -0200

    wash database

--- census/models/wash.js
+++ census/models/wash.js
@@ -23,149 +23,78 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false,
       comment: 'Place this entry belongs to.'
     },
-<<<<<<< HEAD:census/models/submitdata.js
 
     SAM: {
-=======
-    severeacutemalnutrition: {
->>>>>>> 604cdb68d091209e011b8ba5d5678068cdd1a28b:census/models/wash.js
       type: DataTypes.INTEGER,
       allowNull: false,
       comment: ''
     },
-<<<<<<< HEAD:census/models/submitdata.js
     lastUpdateSAM: {
       type: DataTypes.DATE,
       allowNull: true,
-      comment: 'Datetime last update'
+      comment: 'Datetime last update SAM'
     },
     GAN: {
-=======
-    severeacutemalnutritionUpdate: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      comment: ''
-    },
-    globalacutemalnutrition: {
->>>>>>> 604cdb68d091209e011b8ba5d5678068cdd1a28b:census/models/wash.js
       type: DataTypes.INTEGER,
       allowNull: false,
       comment: ''
     },
-<<<<<<< HEAD:census/models/submitdata.js
     lastUpdateGAN: {
       type: DataTypes.DATE,
       allowNull: true,
-      comment: 'Datetime last update'
+      comment: 'Datetime last update GAN'
     },
     ADD: {
-=======
-    globalacutemalnutritionUpdate: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      comment: ''
-    },
-    acutediarrhoealdisease: {
->>>>>>> 604cdb68d091209e011b8ba5d5678068cdd1a28b:census/models/wash.js
       type: DataTypes.INTEGER,
       allowNull: false,
       comment: ''
     },
-<<<<<<< HEAD:census/models/submitdata.js
     lastUpdateADD: {
       type: DataTypes.DATE,
       allowNull: true,
-      comment: 'Datetime last update'
+      comment: 'Datetime last update ADD'
     },
     HWAT: {
-=======
-    acutediarrhoealdiseaseUpdate: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      comment: ''
-    },
-    householdswithouttoilet: {
->>>>>>> 604cdb68d091209e011b8ba5d5678068cdd1a28b:census/models/wash.js
       type: DataTypes.INTEGER,
       allowNull: false,
       comment: ''
     },
-<<<<<<< HEAD:census/models/submitdata.js
     lastUpdateHWAT: {
       type: DataTypes.DATE,
       allowNull: true,
-      comment: 'Datetime last update'
+      comment: 'Datetime last update HWAT'
     },
     HWAW: {
-=======
-    householdswithouttoiletUpdate: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      comment: ''
-    },
-    householdswithoutwater: {
->>>>>>> 604cdb68d091209e011b8ba5d5678068cdd1a28b:census/models/wash.js
       type: DataTypes.INTEGER,
       allowNull: false,
       comment: ''
     },
-<<<<<<< HEAD:census/models/submitdata.js
     lastUpdateHWAW: {
       type: DataTypes.DATE,
       allowNull: true,
-      comment: 'Datetime last update'
+      comment: 'Datetime last update HWAW'
     },
     WSC: {
-=======
-    householdswithoutwaterUpdate: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      comment: ''
-    },
-    watterecoli: {
->>>>>>> 604cdb68d091209e011b8ba5d5678068cdd1a28b:census/models/wash.js
       type: DataTypes.INTEGER,
       allowNull: false,
       comment: ''
     },
-<<<<<<< HEAD:census/models/submitdata.js
     lastUpdateWSC: {
       type: DataTypes.DATE,
       allowNull: true,
-      comment: 'Datetime last update'
+      comment: 'Datetime last update WSC'
     },
     EXND: {
-=======
-    watterecoliUpdate: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      comment: ''
-    },
-    eposuredisasters: {
->>>>>>> 604cdb68d091209e011b8ba5d5678068cdd1a28b:census/models/wash.js
       type: DataTypes.INTEGER,
       allowNull: false,
       comment: ''
     },
-<<<<<<< HEAD:census/models/submitdata.js
     lastUpdateEXND: {
-=======
-    eposuredisastersUpdate: {
->>>>>>> 604cdb68d091209e011b8ba5d5678068cdd1a28b:census/models/wash.js
       type: DataTypes.DATE,
       allowNull: false,
       comment: ''
     },
-<<<<<<< HEAD:census/models/submitdata.js
 
-    createdAt: {
-      type: DataTypes.DATE,
-      allowNull: true,
-      comment: 'Datetime last update'
-    },
-    
-=======
->>>>>>> 604cdb68d091209e011b8ba5d5678068cdd1a28b:census/models/wash.js
     name: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -187,6 +116,11 @@ module.exports = function(sequelize, DataTypes) {
         isEmail: true
       },
       allowNull: false
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      comment: 'Record insert datetime'
     },
     isCurrent: {
       type: DataTypes.BOOLEAN,
