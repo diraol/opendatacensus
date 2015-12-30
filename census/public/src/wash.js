88e42d8d760c46feb9b3a35cd570353cb6f4981a
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2015-12-30 07:34:09 -0500

    Separating wash_submit fronend javascript code

--- census/public/src/wash.js
+++ census/public/src/wash.js
@@ -199,30 +199,6 @@ var printPop = function(placeID) {
 
 $(document).ready(function(){
 
-    if ($('body').hasClass('wash-submit')) {
-
-        $("input.decimal").keydown(function(event) {
-
-            if (event.shiftKey == true)
-                event.preventDefault();
-
-            if ((event.keyCode >= 48 && event.keyCode <= 57) ||
-                (event.keyCode >= 96 && event.keyCode <= 105) ||
-                event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 37 ||
-                event.keyCode == 39 || event.keyCode == 46 || event.keyCode == 190) {
-
-            } else {
-                event.preventDefault();
-            }
-
-            if($(this).val().indexOf('.') !== -1 && event.keyCode == 190)
-                event.preventDefault();
-
-            //if a decimal has been added, disable the "."-button
-        });
-
-    }
-
   // Adds the click listener to the search_bt
   $("#search_bt").on('click', function() {
     currentPlace = $("#place-select").val();
