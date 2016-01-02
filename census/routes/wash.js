5f38db9cd9d0f52567b9e2beec3cd2e1ae74236a
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2016-01-02 07:45:51 -0500

    Removing login requirement from wash form

--- census/routes/wash.js
+++ census/routes/wash.js
@@ -5,13 +5,12 @@ var mixins = require('../controllers/mixins');
 var utils = require('./utils');
 
 var washRoutes = function(router) {
-  var coreMixins = [mixins.requireDomain, mixins.requireAvailableYear,
-    mixins.requireAuth];
+  //var coreMixins = [mixins.requireDomain, mixins.requireAvailableYear,
+  //  mixins.requireAuth];
+  var coreMixins = [mixins.requireDomain];
 
   router.get(utils.scoped('/wash/submit'), coreMixins, wash.submitWash);
   router.post(utils.scoped('/wash/submit'), coreMixins, wash.submitWash);
-  //router.get(utils.scoped('/wash/:id'),[mixins.requireDomain, mixins.requireAvailableYear], wash.pendingEntry);
-  //router.post(utils.scoped('/wash/:id'), coreMixins, wash.reviewPost);
 
   return router;
 };
